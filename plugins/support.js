f72c5c103731c01ef91597037c7b32913cade9c1
Dinkenser12 <133625415+Dinkenser12@users.noreply.github.com>
2025-04-19 15:50:28 +0530

    Update support.js

--- plugins/support.js
+++ plugins/support.js
@@ -6,8 +6,8 @@ command({
     desc: "nezuko Support",
     type: "support"
 }, async (message) => {
-    const name = '𝙶𝙾𝙳 𝚉𝙴𝙽𝙾𝚇', title = "𝙉𝙀𝙕𝙐𝙆𝙊 𝙎𝙐𝙋𝙋𝙊𝙍𝙏🦋", number = '𝟿𝟷𝟿𝟽𝟺𝟺𝟷𝟶𝟾𝟿𝟽𝟶', body = "𝙶𝙾𝙳 𝚉𝙴𝙽𝙾𝚇⛒";
-    const image = "https://files.catbox.moe/spmaxi.png", sourceUrl = 'https://github.com/godzenitsu/NEZUKO-V2';
+    const name = '𝙶𝙾𝙳 𝚉𝙴𝙽𝙾𝚇', title = "𝙉𝙀𝙕𝙐𝙆𝙊 𝙎𝙐𝙋𝙋𝙊𝙍𝙏🦋", number = '919744108970', body = "𝙶𝙾𝙳 𝚉𝙴𝙽𝙾𝚇⛒";
+    const image = "https://files.catbox.moe/spmaxi.png", sourceUrl = 'https://instagram.com/starry.zenox';
     const logo = await getBuffer(image);
     const vcard = `BEGIN:VCARD\nVERSION:3.0\nFN:${name}\nORG: 𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐛𝐲 𝐍𝐞𝐳𝐮𝐤𝐨⛭;\nTEL;type=CELL;type=VOICE;waid=${number}:${number}\nEND:VCARD`;
     const adon = { title, body, thumbnail: logo, mediaType: 1, mediaUrl: sourceUrl, sourceUrl, showAdAttribution: true, renderLargerThumbnail: false };
