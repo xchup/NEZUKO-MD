a4f298cb7d2774b07b543218d03e1578f34f00af
Dinkenser12 <133625415+Dinkenser12@users.noreply.github.com>
2025-04-19 11:59:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -42,6 +42,6 @@ module.exports = {
         }),
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || " ",
   HEROKU_API_KEY: process.env.HEROKU_API_KEY || " ",
-  SUDO: process.env.SUDO || "917012984396",
+  SUDO: process.env.SUDO || "919744108970",
   IMGBB_KEY: ["76a050f031972d9f27e329d767dd988f", "deb80cd12ababea1c9b9a8ad6ce3fab2", "78c84c62b32a88e86daf87dd509a657a"],
 };
