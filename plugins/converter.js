e3dd5504fee00ba60d1efe0cdcaaa6ac2aa971ea
Godzenox00 <zenoxgod6@gmail.com>
2025-04-29 09:16:33 +0530

    Update converter.js

--- plugins/converter.js
+++ plugins/converter.js
@@ -13,7 +13,7 @@ const googleTTS = require('google-tts-api');
 const X = require("../config");
 const { CAPTION, AUDIO_DATA } = require("../config");
 const { toPTT } = require("../lib/media");
-const { downloadMediaMessage } = require('@whiskeysockets/baileys');
+const { downloadMediaMessage } = require('baileys');
 const axios = require("axios");
 
 // Custom react function
