62f3747f71e2e637478394516b6ba779ad32f1b4
Godzenox00 <zenoxgod6@gmail.com>
2025-05-07 22:24:19 +0530

    Removed pp and added fixed one in another file ✅

--- plugins/user.js
+++ plugins/user.js
@@ -74,56 +74,6 @@ you may not use this file except in compliance with the License.
 X-Asena - X-Electra
 */
 
-
-command(
-  {
-    pattern: "pp$",
-    fromMe: true,
-    desc: "Set full screen profile picture",
-    type: "user",
-  },
-  async (message, match,m) => {
-    if (!message.reply_message.image)
-      return await message.reply("*_ʀᴇᴩʟᴀʏ ᴛᴏ ᴀ ɪᴍᴀɢᴇ_*");
-    let media = await m.quoted.download();
-    await updateProfilePicture(message.user, media, message);
-    return await message.reply("*_ᴩʀᴏꜰɪʟᴇ ᴩɪᴄᴛᴜʀᴇ ᴜᴩᴅᴀᴛᴇᴅ_*");
-  }
-);
-
-async function updateProfilePicture(jid, imag, message) {
-  const { query } = message.client;
-  const { img } = await generateProfilePicture(imag);
-  await query({
-    tag: "iq",
-    attrs: {
-      to: jid,
-      type: "set",
-      xmlns: "w:profile:picture",
-    },
-    content: [
-      {
-        tag: "picture",
-        attrs: { type: "image" },
-        content: img,
-      },
-    ],
-  });
-}
-
-async function generateProfilePicture(buffer) {
-  const jimp = await Jimp.read(buffer);
-  const min = jimp.getWidth();
-  const max = jimp.getHeight();
-  const cropped = jimp.crop(0, 0, min, max);
-  return {
-    img: await cropped.scaleToFit(324, 720).getBufferAsync(Jimp.MIME_JPEG),
-    preview: await cropped.normalize().getBufferAsync(Jimp.MIME_JPEG),
-  };
-}
-
-
-
 command(
   {
     pattern: "block",
